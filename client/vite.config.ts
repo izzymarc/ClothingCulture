d37073cdcd3fd37aaad6db0cdbd645b16c19155d
izzymarc <izzymarc@example.com>
2025-03-21 18:52:55 +0100

    Fix asset base path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   },
+  base: './',
   publicDir: 'public'
 })
